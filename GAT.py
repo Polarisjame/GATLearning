7fc04d4fd9a1a5ba052a072f0decb641c7acef4d
Polarisjame <108406757+Polarisjame@users.noreply.github.com>
2022-10-21 17:48:51 +0800

    Update GAT.py

--- GAT.py
+++ GAT.py
@@ -51,68 +51,6 @@ def getModelSize(model):
     return param_size, param_sum, buffer_size, buffer_sum, all_size
 
 
-class GATLayer(nn.Module):
-    def __init__(self, g, in_dim, out_dim):
-        super(GATLayer, self).__init__()
-        self.g = g
-        self.fc = nn.Linear(in_dim, out_dim, bias=False)
-        self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
-        self.res_linear = nn.Linear(in_dim, out_dim)
-        self.rate = torch.nn.Parameter(torch.tensor(0, dtype=torch.float32))
-
-    def edge_attention(self, edges):
-        z2 = torch.cat([edges.src['z'], edges.dst['z']], dim=1)
-        a = self.attn_fc(z2)
-        return {'e': F.leaky_relu(a)}
-
-    def message_func(self, edges):
-        return {'z': edges.src['z'], 'e': edges.data['e']}
-
-    def reduce_func(self, nodes):
-        alpha = F.softmax(nodes.mailbox['e'], dim=1)
-        h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
-        return {'h': h}
-
-    def forward(self, h):
-        z = self.fc(h)  # eq. 1
-        self.g.ndata['z'] = z
-        self.g.apply_edges(self.edge_attention)  # eq. 2
-        self.g.update_all(self.message_func, self.reduce_func)  # eq. 3 and 4
-        h0 = self.res_linear(h)
-        self.g.ndata['h'] += h0 * self.rate
-        return self.g.ndata.pop('h')
-
-
-class MultiHeadGATLayer(nn.Module):
-    def __init__(self, g, in_dim, out_dim, num_heads, merge='cat'):
-        super(MultiHeadGATLayer, self).__init__()
-        self.heads = nn.ModuleList()
-        for i in range(num_heads):
-            self.heads.append(GATLayer(g, in_dim, out_dim))
-        self.merge = merge
-
-    def forward(self, h):
-        head_outs = [attn_head(h) for attn_head in self.heads]
-        if self.merge == 'cat':
-            return torch.cat(head_outs, dim=1)
-        else:
-            return torch.mean(torch.stack(head_outs))
-
-
-class GAT(nn.Module):
-    def __init__(self, g, in_dim, hidden_dim, out_dim, num_heads):
-        super(GAT, self).__init__()
-        self.layer1 = MultiHeadGATLayer(g, in_dim, hidden_dim, num_heads)
-        self.layer2 = MultiHeadGATLayer(g, hidden_dim * num_heads, out_dim, 1)
-
-    def forward(self, h):
-        h = self.layer1(h)
-        h = F.elu(h)
-        h = self.layer2(h)
-        return h
-
-
-# --------------------------------------------
 class GCN_Attention(nn.Module):
     def __init__(self, g, in_feat, out_feat, args):
         super(GCN_Attention, self).__init__()
